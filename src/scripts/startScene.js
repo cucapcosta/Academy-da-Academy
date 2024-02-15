34e867602287ab63e223351b594fc3911545a856
cucapcosta <lucas.costa@sou.inteli.edu.br>
2024-02-15 15:14:49 -0300

    Adicionar tela inicial do jogo

--- src/scripts/startScene.js
+++ src/scripts/startScene.js
@@ -1,10 +1,22 @@
+var fundo;
 class StartScene extends Phaser.Scene {
   constructor() {
     super({ key: "StartScene" });
   }
-  preload() {}
+
+ 
+  preload() {
+    this.load.spritesheet('fundo', '../../assets/gameAssets/startMenu/backgrounds/background.png', {frameWidth: 920, frameHeight: 560})
+  }
   create() {
-    game.stage.backogroundColor = "#000";
+    fundo = this.add.sprite(460, 280, 'fundo')
+    this.anims.create({
+      key: "fundoMove",
+      frames: this.anims.generateFrameNumbers('fundo', {start:0, end: 2}),
+      frameRate: 2,
+      repeat: -1
+    })
+    fundo.anims.play('fundoMove', true);
   }
   update() {}
 }
